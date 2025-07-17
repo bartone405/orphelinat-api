a6ffdec44594dc0f0612b8f60550059c6865e440
bartone405 <barro8453@gmail.com>
2025-07-17 02:52:06 +0200

    Ajout des routes Swagger et Redoc dans urls.py

--- orphelinat_project/urls.py
+++ orphelinat_project/urls.py
@@ -14,9 +14,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+"""
+URL configuration for orphelinat_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import TokenRefreshView
+from orphelinat_app.swagger import schema_view  # Assure-toi que ce fichier existe et exporte schema_view
 
 # Routes API (pas de format_suffix_patterns ici !)
 api_urlpatterns = [
@@ -27,6 +45,11 @@ api_urlpatterns = [
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Routes API directement incluses sans format_suffix_patterns
+    # Routes API incluses sous /api/
     *api_urlpatterns,
+
+    # Swagger & Redoc accessibles à la racine
+    re_path(r'^swagger(?P<file_format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
